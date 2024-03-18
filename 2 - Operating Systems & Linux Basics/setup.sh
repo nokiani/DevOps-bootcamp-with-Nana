b248a1679d7b0a1030df634a6a6aa01209a09e51
Alexey Zolotarev <alexey.zolotareff@gmail.com>
2024-03-18 13:14:13 +0500

    Added user input

--- 2 - Operating Systems & Linux Basics/setup.sh
+++ 2 - Operating Systems & Linux Basics/setup.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-echo "Setup and configure server"
+echo "Reading user input"
 
-file_name=config.yaml
+read -r -p "Please enter your password: " user_pwd
 
-config_dir=$1
-
-if [ -d "$config_dir" ]
-then
-  echo "reading config directory contents"
-  config_files=$(ls "$config_dir")
-else
-  echo "config dir not found. Creating one"
-  mkdir "$config_dir"
-  touch "$config_dir/config.sh"
-fi
-
-user_group=$2
-
-if [ "$user_group" == "alex" ]
-then
-  echo "configure the server"
-elif [ "$user_group" == "admin" ]
-then
-  echo "administer the server"
-else
-  echo "No permission to configure sever. Wrong user group"
-fi
-
-
-echo "using file $file_name to configure something"
-
-echo "here are all configuration files: $config_files"
+echo "thanks for your password $user_pwd"
