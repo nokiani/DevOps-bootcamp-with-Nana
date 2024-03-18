6360255a000347a6b303e522085dbd8a5773a8f8
Alexey Zolotarev <alexey.zolotareff@gmail.com>
2024-03-18 12:36:52 +0500

    Echo error

--- 2 - Operating Systems & Linux Basics/setup.sh
+++ 2 - Operating Systems & Linux Basics/setup.sh
@@ -2,3 +2,9 @@
 
 echo "Setup and configure server"
 
+file_name=config.yaml
+config_files=$(ls config)
+
+echo "using file $file_name to configure something"
+
+echo "here are all configuration files: $config_files"
