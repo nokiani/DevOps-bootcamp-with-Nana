27dac1ce512dfce57d60d3d458a96b5d5b8890a4
Alexey Zolotarev <alexey.zolotareff@gmail.com>
2024-03-18 15:41:34 +0500

    Added boolean to function

--- 2 - Operating Systems & Linux Basics/setup.sh
+++ 2 - Operating Systems & Linux Basics/setup.sh
@@ -36,12 +36,19 @@ function score_sum {
 
 function create_file() {
   file_name=$1
+  is_shell_script=$2
   touch "$file_name"
   echo "file $file_name created"
+
+  if [ "$is_shell_script" = true ]
+  then
+    chmod u+x "$file_name"
+    echo "added execute permission"
+  fi
 }
 
 create_file test.txt
 
 create_file myfile.yaml
 
-create_file myscript.sh
+create_file myscript.sh true
