f75e856743574b6b05785514a2ac942e07fb2846
Alexey Zolotarev <alexey.zolotareff@gmail.com>
2024-03-18 13:34:37 +0500

    Loop statements

--- 2 - Operating Systems & Linux Basics/setup.sh
+++ 2 - Operating Systems & Linux Basics/setup.sh
@@ -6,5 +6,26 @@ echo "all params: $*"
 # Total number of arguments provided
 echo "nubmer of params: $#"
 
-echo "user $1"
-echo "group $2"
+for param in $*
+  do
+    if [ -d "$param" ]
+    then
+      echo "executing scripts in the config folder"
+      ls -l "$param"
+    fi
+  done
+
+sum=0
+while true
+  do
+    read -r -p "enter a score: " score
+
+    if [ "$score" == "q" ]
+    then
+      break
+    fi
+
+    sum=$((sum+score))
+    echo "total score: $sum"
+
+  done
